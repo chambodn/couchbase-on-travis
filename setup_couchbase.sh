2526a20120dfaec6bbe2b3205fc90584fa787e1a
chambodn <chambodn@skysoft-atm.com>
2018-10-23 19:59:19 +0200

    try soething else

--- setup_couchbase.sh
+++ setup_couchbase.sh
@@ -11,18 +11,11 @@ export CB_PACKAGE=couchbase-server-community_5.1.1-ubuntu16.04_amd64.deb
 export CB_SHA256=b8d15af64710c61f8a98218c632becb400feec8a99a593f8e76aa3320fa58bbb
 
 echo "Prepare Couchbase dependencies"
+
+curl -O http://packages.couchbase.com/releases/couchbase-release/couchbase-release-1.0-4-amd64.deb
+sudo dpkg -i couchbase-release-1.0-4-amd64.deb
+
 sudo apt-get update 
-sudo apt-get install -yq libssl1.0.0 runit wget python-httplib2 chrpath tzdata lsof lshw sysstat net-tools numactl
+sudo apt-get install -yq libssl1.0.0 
 
-echo "Downloading couchbase"
-wget -q -N $CB_RELEASE_URL/$CB_VERSION/$CB_PACKAGE
-echo "$CB_SHA256  $CB_PACKAGE" | sha256sum -c -
-sudo dpkg -i ./$CB_PACKAGE && rm -f ./$CB_PACKAGE
-
-echo "Wait for couchbase startup"
-sleep 10
-
-echo "Create [ admin ] user"
-sudo /opt/couchbase/bin/couchbase-cli cluster-init -c localhost:8091 -u admin -p password --cluster-ramsize=256 --service=data,index,query
-
-echo "Couchbase running successfully"
+sudo apt install .couchbase-server.deb
