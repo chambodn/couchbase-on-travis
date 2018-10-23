2e7798cdd4ac777c3aac615bf5df4e2175a70096
chambodn <chambodn@skysoft-atm.com>
2018-10-23 20:15:23 +0200

    Remove checksum

--- setup_couchbase.sh
+++ setup_couchbase.sh
@@ -27,13 +27,11 @@
  export CB_VERSION=5.1.1
 export CB_RELEASE_URL=https://packages.couchbase.com/releases
 export CB_PACKAGE=couchbase-server-community_5.1.1-ubuntu14.04_amd64.deb
-export CB_SHA256=b8d15af64710c61f8a98218c632becb400feec8a99a593f8e76aa3320fa58bbb
  echo "Prepare Couchbase dependencies"
 sudo apt-get update 
 sudo apt-get install -yq libssl1.0.0 runit wget python-httplib2 chrpath tzdata lsof lshw sysstat net-tools numactl
  echo "Downloading couchbase"
 wget -q -N $CB_RELEASE_URL/$CB_VERSION/$CB_PACKAGE
-echo "$CB_SHA256  $CB_PACKAGE" | sha256sum -c -
 sudo dpkg -i ./$CB_PACKAGE && rm -f ./$CB_PACKAGE
  echo "Wait for couchbase startup"
 sleep 10
